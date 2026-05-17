6a96b866fcffd3b843135c80fc5e46edc67cc803
VeeyselDemir <veeyseldemir33@gmail.com>
2026-05-17 21:28:33 +0300

    Create Add_Products.sql

---
+++ Veri Girişi Sorguları/Add_Products.sql
@@ -0,0 +1,8 @@
+INSERT INTO Products (ProductID, RestaurantID, ProductName, Price, IsActive)
+VALUES
+(1, 'Adana Kebap', 350.00, 1),
+(1, 'Urfa Kebap', 340.00, 1),
+(1, 'Mercimek Çorbası', 75.00, 1),
+(2, 'Kıymalı Pide', 200.00, 1),
+(2, 'Lahmacun', 100.00, 1),
+(2, 'Kutu Kola', 40.00, 1);
