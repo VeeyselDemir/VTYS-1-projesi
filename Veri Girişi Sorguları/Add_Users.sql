8f02dbb43818f854e4f62904d935ff26cb9823ee
VeeyselDemir <veeyseldemir33@gmail.com>
2026-05-17 21:26:20 +0300

    Create Add_Users.sql

---
+++ Veri Girişi Sorguları/Add_Users.sql
@@ -0,0 +1,10 @@
+INSERT INTO Users (UserID, FullName, Email, Phone, PasswordHash, UserType, IsVerified, IsActive)
+VALUES
+('Ahmet Yılmaz', 'ahmet@gmail.com', '05551234567', 'Sifre123', 'Musteri', 1, 1),
+('Ayşe Kaya', 'ayse@gmail.com', '05329876543', 'Sifre456', 'Musteri', 1, 1),
+('Mehmet Demir', 'mehmet@gmail.com', '05441112233', 'Sifre789', 'IhtiyacSahibi', 1, 1),
+('Zeynep Arslan', 'zeynep@gmail.com', '05567891234', 'Sifre111', 'Musteri', 1, 1),
+('Burak Şahin', 'burak@gmail.com', '05341239876', 'Sifre222', 'Kurye', 1, 1),
+('Elif Çelik', 'elif@gmail.com', '05452346789', 'Sifre333', 'IhtiyacSahibi', 1, 1),
+('Murat Koç', 'murat@gmail.com', '05533445566', 'Sifre444', 'Musteri', 1, 1),
+('Seda Aydın', 'seda@gmail.com', '05398765432', 'Sifre555', 'RestoranYetkilisi', 1, 1);
