00a8053375c43c5f4e9e9b35749309036cfd9850
VeeyselDemir <veeyseldemir33@gmail.com>
2026-05-17 21:35:16 +0300

    Update Add_Orders.sql

--- Veri Girişi Sorguları/Add_Orders.sql
+++ Veri Girişi Sorguları/Add_Orders.sql
@@ -1,4 +1,4 @@
-INSERT INTO Orders (OrderId, UserId, OrderDate, TotalAmount, OrderStatus, IsActive)
+INSERT INTO Orders (UserId, OrderDate, TotalAmount, OrderStatus, IsActive)
 VALUES
 (2, '2026-05-16 21:58:12.120', 400.00, 'Completed', 1),
 (3, '2026-05-16 22:32:45.273', 500.00, 'Completed', 1),
