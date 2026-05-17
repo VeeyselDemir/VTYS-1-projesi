d186d424b0c6bfa047ad8a73f32ed948430b5e95
VeeyselDemir <veeyseldemir33@gmail.com>
2026-05-17 20:34:54 +0300

    Create vw_AktifAskidaYemekler.sql

---
+++ Görünümler/vw_AktifAskidaYemekler.sql
@@ -0,0 +1,15 @@
+CREATE VIEW vw_AktifAskidaYemekler
+AS
+SELECT 
+    dp.DonationId,
+    p.ProductName AS AskidakiYemek,
+    CASE 
+        WHEN dp.IsAnonymous = 1 THEN 'Gizli Bağışçı'
+        ELSE u.FullName 
+    END AS BagisciAdi,
+    dp.DonationDate AS BagisTarihi,
+    dp.PoolStatus AS Durum
+FROM DonationPool dp
+INNER JOIN Products p ON dp.ProductId = p.ProductID
+LEFT JOIN Users u ON dp.DonorUserId = u.UserID
+WHERE dp.PoolStatus = 'Active'; -- Durum kodunuza göre 'Bekliyor' vs. yapabilirsiniz.
