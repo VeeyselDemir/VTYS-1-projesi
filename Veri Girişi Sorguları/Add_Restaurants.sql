979e054179ba4b8f31bc50c6b2831e001f752c98
VeeyselDemir <veeyseldemir33@gmail.com>
2026-05-17 21:27:13 +0300

    Create Add_Restaurants.sql

---
+++ Veri Girişi Sorguları/Add_Restaurants.sql
@@ -0,0 +1,4 @@
+INSERT INTO Restaurants (RestaurantID, RestaurantName, Address, Rating, IsActive)
+VALUES
+('Lezzet Kebapçısı', 'ValimithatBey mahallesi Ekinci Sokak', 4.5, 1),
+('Menzil Pide & Lahmacun', 'Merkez Mahallesi 3. Sokak', 4.2, 1);
